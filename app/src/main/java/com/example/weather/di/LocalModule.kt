65c2a96db1b1df92047db1d84142efdcd1624906
Hussien <sonictwester@gmail.com>
2023-02-25 18:51:26 +0200

    added db module

---
+++ app/src/main/java/com/example/weather/di/LocalModule.kt
@@ -0,0 +1,22 @@
+package com.example.weather.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.data.local.FavDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalModule {
+    @Provides
+    @Singleton
+    fun provideDB(@ApplicationContext context: Context)= Room.databaseBuilder(context,FavDatabase::class.java,"fav_db").build()
+    @Provides
+    @Singleton
+    fun provideDao(db:FavDatabase)=db.favDao()
+}
--- domain/src/main/java/com/example/domain/repo/WeatherRepo.kt
+++ domain/src/main/java/com/example/domain/repo/WeatherRepo.kt
@@ -1,7 +1,8 @@
 package com.example.domain.repo
 
 import com.example.domain.entity.forcast.ForcastResponse
-import com.example.domain.entity.onecall.WeatherResponse
+import com.example.domain.entity.weather.WeatherResponse
+
 
 interface WeatherRepo {
     fun getWeatherFromRemote(lat:Double,lon:Double,units:String,lang:String,apiKey:String): WeatherResponse
