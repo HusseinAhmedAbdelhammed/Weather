236bb43582f8a9bdfa0e13512515d19ee976cd21
Hussien <sonictwester@gmail.com>
2023-02-25 10:28:51 +0200

    added usecase api injection

--- app/src/main/java/com/example/weather/di/UseCaseModule.kt
+++ app/src/main/java/com/example/weather/di/UseCaseModule.kt
@@ -1,5 +1,23 @@
 package com.example.weather.di
 
+import com.example.domain.repo.WeatherRepo
+import com.example.domain.usecase.GetForcast
+import com.example.domain.usecase.GetWeather
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 object UseCaseModule {
-    //we stopped here as use cases still needs other parameters
+    
+    @Provides
+    fun provideUseCaseWeath(weatherRepo: WeatherRepo,lat:Double,lon:Double,unit:String,lang:String,apiKey:String):GetWeather{
+        return GetWeather(weatherRepo,lat,lon,unit,lang,apiKey)
+    }
+    @Provides
+    fun provideUseCaseForeCast(weatherRepo: WeatherRepo,lat: Double,lon: Double,apiKey: String):GetForcast{
+        return GetForcast(weatherRepo,lat,lon,apiKey)
+    }
 }
--- data/src/main/java/com/example/data/repo/WeatherRepoImplementation.kt
+++ data/src/main/java/com/example/data/repo/WeatherRepoImplementation.kt
@@ -2,7 +2,7 @@ package com.example.data.repo
 
 import com.example.data.remote.ApiService
 import com.example.domain.entity.forcast.ForcastResponse
-import com.example.domain.entity.onecall.WeatherResponse
+import com.example.domain.entity.weather.WeatherResponse
 import com.example.domain.repo.WeatherRepo
 
 class WeatherRepoImplementation(private val apiService: ApiService):WeatherRepo {
